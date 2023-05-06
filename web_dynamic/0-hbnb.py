f1870c5bbd04f491b79a4aadb761fe78717408ca
COLLINS  TONUI <tckiprotich@gmail.com>
2023-05-06 11:35:49 +0000

    uuid

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -34,6 +34,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
+    cache_id = uuid.uuid4()
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
